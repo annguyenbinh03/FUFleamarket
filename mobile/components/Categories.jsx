0df5ed5e65cb747a0702b81158afe6c823a24a67
Ho Minh Quyen <minhquyenho09@gmail.com>
2024-06-20 14:52:02 +0700

    Update mobile: Add folder categories image, Update Image in Categories.jsx

--- mobile/components/Categories.jsx
+++ mobile/components/Categories.jsx
@@ -10,6 +10,7 @@ import {
   Dimensions,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { images } from "../constants";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
@@ -17,17 +18,13 @@ const Categories = () => {
   const navigation = useNavigation();
 
   const categoryImages = {
-    1: "https://th.bing.com/th/id/OIP.BGwhDC-5eqhHHqZkDPDMygHaE7?rs=1&pid=ImgDetMain",
-    2: "https://th.bing.com/th/id/OIP.h8ad_GoFNr756ufq9-aaNwAAAA?pid=ImgDet&w=204&h=175&c=7",
-    3: "https://th.bing.com/th/id/OIP.eCRz4IvORHX8GuPK9QtnBQAAAA?rs=1&pid=ImgDetMain",
-
-    4: "https://th.bing.com/th/id/R.d5e39142b9c02fd27a0323ea09f50294?rik=1IKuzLMieHFNrA&pid=ImgRaw&r=0",
-
-    5: "https://th.bing.com/th/id/OIP.WRjgN9bpfQAMR9FG4RVUQgHaI3?rs=1&pid=ImgDetMain",
-
-    6: "https://i.pinimg.com/originals/b0/9e/ee/b09eee4a864f044d7616cf3fd8271279.jpg",
-    7: "https://th.bing.com/th/id/OIP.jxfGNxjhixVX3BMtJ_H4vgAAAA?rs=1&pid=ImgDetMain",
-    8: "https://th.bing.com/th/id/OIP.ID8Wa0zGXGAaRuqICpPyoAHaER?w=323&h=186&c=7&r=0&o=5&pid=1.7",
+    1: images.dodientu,
+    2: images.dodunghoctap,
+    3: images.dienlanh,
+    4: images.donoithat,
+    5: images.thucpham,
+    6: images.thoitrang,
+    7: images.giaitri,
   };
 
   useEffect(() => {
@@ -66,11 +63,7 @@ const Categories = () => {
             >
               <View>
                 <Image
-                  source={{
-                    uri:
-                      categoryImages[item.categoryId] ||
-                      "https://lighthouse.chotot.com/_next/image?url=https%3A%2F%2Fstatic.chotot.com%2Fstorage%2Fchapy-pro%2Fnewcats%2Fv8%2F5000.png&w=256&q=95",
-                  }}
+                  source={categoryImages[item.categoryId]}
                   style={styles.categoryPic}
                 />
               </View>
--- mobile/constants/images.js
+++ mobile/constants/images.js
@@ -5,5 +5,27 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import dienlanh from "../assets/categories/dienlanh.jpg";
+import dodientu from "../assets/categories/dodientu.jpg";
+import dodunghoctap from "../assets/categories/dodunghoctap.jpg";
+import donoithat from "../assets/categories/donoithat.jpg";
+import giaitri from "../assets/categories/giaitri.jpg";
+import thoitrang from "../assets/categories/thoitrang.jpg";
+import thucpham from "../assets/categories/thucpham.jpg";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default {
+  profile,
+  thumbnail,
+  cards,
+  path,
+  logo,
+  logoSmall,
+  empty,
+  dienlanh,
+  dodientu,
+  dodunghoctap,
+  donoithat,
+  giaitri,
+  thoitrang,
+  thucpham,
+};
