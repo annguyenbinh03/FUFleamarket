42df3543623053289e2b4e4955abcb951847901e
DanThuSE170446 <thuphamnguyendan@gmail.com>
2024-06-24 22:24:26 +0700

    remove unnecessary imports in Promotion Transaction Repository

--- TestFUFM/Repository/PromotionTransactionRepository.cs
+++ TestFUFM/Repository/PromotionTransactionRepository.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Repository.Implementations
+namespace Repository
 {
     public class PromotionTransactionRepository : IPromotionTransactionRepository
     {
